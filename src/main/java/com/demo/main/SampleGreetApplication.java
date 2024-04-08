0db696dc8c7efb3381e2bad0428b7958a70904c8
kirankumarkommu007 <kommukirankumar007@gmail.com>
2024-04-08 15:26:25 +0530

    first upload

---
+++ src/main/java/com/demo/main/SampleGreetApplication.java
@@ -0,0 +1,13 @@
+package com.demo.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleGreetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleGreetApplication.class, args);
+	}
+
+}
